3721aaec65c14ad22b01f1c55c6176669aebda93
wahyurahmaniar <rahmaniar.w.aa.titech@gmail.com>
2026-08-15 23:10:09 +0900

    Update education.js

--- data/education.js
+++ data/education.js
@@ -3,21 +3,18 @@ var EDU = [
     deg: 'Ph.D. Electrical Engineering',
     school: 'National Central University, Taiwan',
     desc: 'Real-time object detection, computer vision, artificial intelligence, medical imaging',
-    year: '2014&ndash;2020',
     featured: true
   },
   {
     deg: '(Fastrack master degree)',
     school: 'National Central University, Taiwan',
     desc: 'UAV robotic, sensors integration, computer vision',
-    year: '2013&ndash;2014',
     featured: false
   },
   {
     deg: 'B.Sc. Electronics and Instrumentation',
     school: 'Universitas Gadjah Mada, Indonesia',
     desc: 'Mobile robots, telemetry, sensors, interface',
-    year: '2005&ndash;2009',
     featured: false
   }
 ];

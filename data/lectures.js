867f0c8ac2f4d026dce4b4caee987dae223b0b46
wahyurahmaniar <rahmaniar.w.aa.titech@gmail.com>
2026-08-15 23:05:12 +0900

    Update lectures.js

--- data/lectures.js
+++ data/lectures.js
@@ -23,7 +23,7 @@ img: null,
 slides: 'https://docs.google.com/presentation/d/e/2PACX-1vR3mn70Wvc0JelzMbTo7vtDbQ7iBY8cYHUnEpAem2IoGPhcZwgphnLGCukbqWZPbg/pubembed?start=true&loop=false&delayms=3000'},
 
 {t:'Guest Lecture: Artificial Intelligence (AI) in Medical Imaging',
-v:'Universitas Brawijaya', y:'2026', ty:'tl', tl:'Lecture',
+v:'Universitas Brawijaya (UB)', y:'2026', ty:'tl', tl:'Lecture',
 links:['Slides'],
 img: null,
 slides: ''},
